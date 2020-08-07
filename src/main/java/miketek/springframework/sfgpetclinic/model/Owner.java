fd744a26a1a47cf6d81cec9861841a0b2a08cde5
Miguel Suarez <miguel.suarez@nearshoretechnology.com>
2020-08-06 20:57:43 -0500

    Implement Pet Clinic POJO Data Model #2

---
+++ src/main/java/miketek/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package miketek.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/miketek/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package miketek.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
